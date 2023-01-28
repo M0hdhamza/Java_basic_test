5a3f01c0d168c58c84886a052537b9e59e4d9e52
hemurock <chsai949444@gmail.com>
2023-01-28 12:01:31 +0530

    Data Types

---
+++ src/dataTypes/DataTypes.java
@@ -0,0 +1,19 @@
+package dataTypes;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		byte a = -50;
+		short b = 100;
+		int c = 250;
+		long d = 10000;
+		char e = 'A';
+		double f = 1111.000;
+		float g = 12f;
+		byte h = 20;
+		int result = a + h;
+	System.out.println(result);
+	}
+	
+
+}
---
+++ src/dataTypes/Implicit.java
@@ -0,0 +1,15 @@
+package dataTypes;
+
+public class Implicit {
+
+	public static void main(String[] args) {
+		byte a=100;
+		int b=a;
+		System.out.println(b);
+		long i=b;
+		System.out.println(i);
+		char ch='a';
+		int x=ch;
+		System.out.println(x);
+	}
+}
