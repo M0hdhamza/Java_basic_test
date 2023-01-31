d0969947882607287727c821d9aa57c8e1775cea
hemurock <chsai949444@gmail.com>
2023-01-31 10:13:09 +0530

    UptoString

--- src/WrapperClass/StringAndObject.java
+++ src/WrapperClass/StringAndObject.java
@@ -6,5 +6,12 @@ public class StringAndObject {
 		long g=1000000L;
 		Long y=Long.valueOf(g);
 		String s=y.toString();
+		
+		byte c=100;
+		Byte z= Byte.valueOf(c);
+		String u=Byte.toString(z);
+		System.out.println(z);
+		System.out.println(u);
+		
 	}
 }
